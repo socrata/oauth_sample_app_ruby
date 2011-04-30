4ce20301c179bfb75c745f9e683a093d6cce05b3
Clint Tseng <clint@dontexplain.com>
2011-04-29 18:23:38 -0700

    explain better why the serializer middleware must exist.

--- socrata_request_serializer_middleware.rb
+++ socrata_request_serializer_middleware.rb
@@ -1,6 +1,18 @@
 require 'json'
 require 'faraday'
 
+# the OAuth2 rubygem insists on adding the access_token to not only the header,
+# but also in the http params, whether as a GET param on in the POST body. we
+# don't want this behavior, because SODA only ever looks in the Authorization
+# header, and can get confused if you attempt to give it a parameter of
+# access_token when it isn't expecting it. luckily, the OAuth2 gem also uses
+# faraday to send its requests, which means that we can easily supplant their
+# behavior with our own if we add some middleware to filter the content before
+# we send it off. that is what this class does; if we are authorized, set our
+# content-type to be json for convenience, delete the access_token parameter
+# from the params body, and the serialize the remainder (which we actually
+# wanted to send) to json before sending it all off to SODA.
+
 class SocrataRequestSerializerMiddleware < Faraday::Middleware
   def call(env)
     # serialize to json, but only if we're already authorized.
